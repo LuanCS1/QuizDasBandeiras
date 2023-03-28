9e4ec9788a90ddb34515175aa39765679bbed30d
Luan Carlos dos Santos <luan.santos@olos.com.br>
2023-03-27 22:00:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/quizdasbandeiras/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.quizdasbandeiras;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtNome;
+    private Button btnIniciar;
+    private int pontos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtNome = findViewById(R.id.edtNome);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+        edtNome.setOnKeyListener((view, i, keyEvent) -> {
+            if (edtNome.getText().length() > 1) {
+                btnIniciar.setEnabled(true);
+
+                return true;
+            } else if (edtNome.getText().length() == 0) {
+                btnIniciar.setEnabled(false);
+            }
+
+            return false;
+        });
+    }
+
+    public void startQuiz(View view) {
+        Intent it = new Intent(this, ScreenBrazil.class);
+
+        it.putExtra("user", edtNome.getText().toString());
+        it.putExtra("pontos", pontos);
+
+        startActivity(it);
+    }
+
+    public void sairQuiz(View view){
+
+        finishAffinity();
+
+    }
+}
---
+++ app/src/main/java/com/example/quizdasbandeiras/ScreenBrazil.java
@@ -0,0 +1,50 @@
+package com.example.quizdasbandeiras;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScreenBrazil extends AppCompatActivity {
+    private RadioGroup rbgContent;
+    private Button btnResponse;
+
+    private TextView textTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_brazil);
+
+        rbgContent = findViewById(R.id.rbgContent);
+        btnResponse = findViewById(R.id.btnResponse);
+        TextView textTitle = findViewById(R.id.textTitle);
+    }
+
+
+    public void OnRadioButtonClicked (View view){
+        btnResponse.setEnabled(true);
+    }
+
+    public void Confirm (View view){
+        int idChecked = rbgContent.getCheckedRadioButtonId();
+
+        int pontos = getIntent().getIntExtra("pontos", 0);
+        String user = getIntent().getStringExtra("user");
+
+        if (idChecked == R.id.rbBrazil) {
+            pontos++;
+        }
+        Intent it = new Intent(this, ScreenBulgaria.class);
+
+        it.putExtra("user", user);
+        it.putExtra("pontos", pontos);
+
+        startActivity(it);
+        finish();
+    }
+}
